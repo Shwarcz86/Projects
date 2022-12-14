b0a2f94cdac74c43372e7454c27633883717da06
Shwarcz86 <shwarcz@mail.ru>
2022-12-14 15:08:10 +0300

    fix_slider

--- Biodynamic/js/app.js
+++ Biodynamic/js/app.js
@@ -4269,6 +4269,10 @@
             speed: 1e3,
             parallax: true,
             direction: "vertical",
+            autoplay: {
+                delay: 3e3,
+                disableOnInteraction: false
+            },
             pagination: {
                 el: ".main-slider__control",
                 clickable: true
