865fd2c5575c9bdeb38e5f2f62a4a07d4701aaee
Shwarcz86 <shwarcz@mail.ru>
2023-02-07 16:23:41 +0300

    fix_2

--- Rothschild/js/animate.js
+++ Rothschild/js/animate.js
@@ -1,135 +1,138 @@
-gsap.registerPlugin(ScrollTrigger)
-const tl = gsap.timeline()
+if (window.matchMedia('(min-width: 1024.98px)').matches) {
 
-tl.fromTo(
-    '.main-page__img', 
-    {
-        opacity: 0,
-        scale: 0.5,
+    gsap.registerPlugin(ScrollTrigger)
+    const tl = gsap.timeline()
 
-    }, 
-    {
-        opacity: 1,
-        scale: 1,
-        duration: 1
-    },
-)
-    .fromTo(
-        '.main-page__title', 
+    tl.fromTo(
+        '.main-page__img', 
         {
-            x: -200,
             opacity: 0,
+            scale: 0.5,
+
         }, 
         {
-            x: 0,
             opacity: 1,
-            duration: 1
-        },
-        1,
-    )
-    .fromTo(
-        '.main-page__subtitle', 
-        {
-            y: 50,
-            opacity: 0
-        }, 
-        {
-            y: 0,
-            opacity: 1,
-            duration: 1
-        },
-    )
-    .fromTo(
-        '.main-page__body',
-        {
-            x: 300,
-            opacity: 0
-        },
-        {
-            x: 0,
-            opacity: 1,
-            duration: 1
-        },
-        2.5,
-    )
-    .fromTo(
-        '.top-header__inner',
-        {
-            y: -50,
-            opacity: 0
-        },
-        {
-            y: 0,
-            opacity: 1,
-            duration: 1
-        },
-        3.5,
-    )
-    .fromTo(
-        '.bottom-header',
-        {
-            opacity: 0,
-        },
-        {
-            opacity: 1,
-            duration: 1,
-        },
-        4.5,
-    )
-    .fromTo(
-        '.bottom-header__menu li',
-        {
-            y: -50,
-            opacity: 0,
-        },
-        {
-            y: 0,
-            opacity: 1,
-            duration: 1,
-            stagger: 0.1,
-        },
-        4.5,
-    )
-    .fromTo(
-        '.main-page__social-item',
-        {
-            scale: 0,
-            opacity: 0
-        },
-        {
             scale: 1,
-            opacity: 1,
-            duration: 1,
+            duration: 1
         },
-        6
     )
+        .fromTo(
+            '.main-page__title', 
+            {
+                x: -200,
+                opacity: 0,
+            }, 
+            {
+                x: 0,
+                opacity: 1,
+                duration: 1
+            },
+            1,
+        )
+        .fromTo(
+            '.main-page__subtitle', 
+            {
+                y: 50,
+                opacity: 0
+            }, 
+            {
+                y: 0,
+                opacity: 1,
+                duration: 1
+            },
+        )
+        .fromTo(
+            '.main-page__body',
+            {
+                x: 300,
+                opacity: 0
+            },
+            {
+                x: 0,
+                opacity: 1,
+                duration: 1
+            },
+            2.5,
+        )
+        .fromTo(
+            '.top-header__inner',
+            {
+                y: -50,
+                opacity: 0
+            },
+            {
+                y: 0,
+                opacity: 1,
+                duration: 1
+            },
+            3.5,
+        )
+        .fromTo(
+            '.bottom-header',
+            {
+                opacity: 0,
+            },
+            {
+                opacity: 1,
+                duration: 1,
+            },
+            4.5,
+        )
+        .fromTo(
+            '.bottom-header__menu li',
+            {
+                y: -50,
+                opacity: 0,
+            },
+            {
+                y: 0,
+                opacity: 1,
+                duration: 1,
+                stagger: 0.1,
+            },
+            4.5,
+        )
+        .fromTo(
+            '.main-page__social-item',
+            {
+                scale: 0,
+                opacity: 0
+            },
+            {
+                scale: 1,
+                opacity: 1,
+                duration: 1,
+            },
+            6
+        )
 
-// Scroll animation
-gsap.to('.main-page__inner', {
-    scrollTrigger: {
-        trigger: '.main-page',
-        start: 'top top',
-        scrub: true,
-    },
+    // Scroll animation
+    gsap.to('.main-page__inner', {
+        scrollTrigger: {
+            trigger: '.main-page',
+            start: 'top top',
+            scrub: true,
+        },
 
-    yPercent: 50,
-    scale: 0.5,
-    xPercent: -50,
-    opacity: 0,
-})
-gsap.to('.main-page__bg', {
-    scrollTrigger: {
-        trigger: '.main-page',
-        start: 'top top',
-        scrub: true,
-    },
-   scale: 2,
-})
-gsap.to('.main-page__img img', {
-    scrollTrigger: {
-        trigger: '.main-page',
-        start: 'top top',
-        scrub: true,
-    },
-    opacity: 0.5
-})
+        yPercent: 50,
+        scale: 0.5,
+        xPercent: -50,
+        opacity: 0,
+    })
+    gsap.to('.main-page__bg', {
+        scrollTrigger: {
+            trigger: '.main-page',
+            start: 'top top',
+            scrub: true,
+        },
+    scale: 2,
+    })
+    gsap.to('.main-page__img img', {
+        scrollTrigger: {
+            trigger: '.main-page',
+            start: 'top top',
+            scrub: true,
+        },
+        opacity: 0.5
+    })
+}
